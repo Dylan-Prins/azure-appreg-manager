16fc18a2f6291b828793f2af3b479d13e36ef4a3
Dylan Prins <8777848+Dylan-Prins@users.noreply.github.com>
2024-01-23 23:18:38 +0100

    its something

--- full.ps1
+++ full.ps1
@@ -1,6 +1,31 @@
-Import-Module -Name Pode,Pode.Web
+Import-Module -Name Pode, Pode.Web
+
+$env:UserPrincipalName = "dylan.prins@insparklabs.nl"
 
 Start-PodeServer {
     Add-PodeEndpoint -Address * -Port 443 -Protocol Https -Name User -SelfSigned
     Use-PodeWebTemplates -Title 'test' -Theme Dark
+
+    Add-PodeWebPage -Name 'AppRegistrations' -Icon 'Settings' -ScriptBlock {
+        New-PodeWebCard -Content @(
+            New-PodeWebTable -Name 'App Registrations' -DataColumn AppId -Click -ScriptBlock {
+                Get-MgApplication -All | ForEach-Object {
+                    $owners = Get-MgApplicationOwnerAsUser -ApplicationId $_.Id | Where-Object { $_.UserPrincipalName -eq $env:UserPrincipalName }
+                    if ($null -ne $owners) {
+                        [PSCustomObject]@{
+                            Name   = $_.DisplayName
+                            AppId  = $_.AppId
+                            Owners = $Owners.DisplayName
+                        }
+                    }
+                }
+            } -ClickScriptBlock {
+                [PSCustomObject]@{
+                    Name   = 'stuf'
+                    AppId  = 'more stuf'
+                    Owners = 'even more stuf'
+                }
+            }
+        )
+    }
 }
