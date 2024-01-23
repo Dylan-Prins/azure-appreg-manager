fd7589cbfc939a0de10059f4c2e8422558ebc2ad
Dylan Prins <8777848+Dylan-Prins@users.noreply.github.com>
2024-01-23 20:04:41 +0000

    testing local

--- full.ps1
+++ full.ps1
@@ -1,6 +1,6 @@
-Import-Module -Name Pode.Web
+Import-Module -Name Pode,Pode.Web
 
 Start-PodeServer {
-    Add-PodeEndpoint -Address * -Port 8090 -Protocol Http -Name User
+    Add-PodeEndpoint -Address * -Port 443 -Protocol Https -Name User -SelfSigned
     Use-PodeWebTemplates -Title 'test' -Theme Dark
 }
